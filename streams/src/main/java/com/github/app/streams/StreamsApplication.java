21a9d51237fce13a5596abcdb20e7f2b0aca1fcb
Qunfei Wu <wu.qunfei@gmail.com>
2023-10-01 13:17:05 +0200

    update color and ide settings

--- streams/src/main/java/com/github/app/streams/StreamsApplication.java
+++ streams/src/main/java/com/github/app/streams/StreamsApplication.java
@@ -2,8 +2,6 @@ package com.github.app.streams;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.kafka.annotation.EnableKafka;
-import org.springframework.kafka.annotation.EnableKafkaStreams;
 
 @SpringBootApplication
 public class StreamsApplication {
