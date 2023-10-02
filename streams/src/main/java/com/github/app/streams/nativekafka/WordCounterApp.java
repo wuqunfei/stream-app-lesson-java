d3966e15db694492e0d091d4da24ccc54774fb2d
Qunfei Wu <wu.qunfei@gmail.com>
2023-10-02 17:43:15 +0200

    adding update

--- streams/src/main/java/com/github/app/streams/nativekafka/WordCounterApp.java
+++ streams/src/main/java/com/github/app/streams/nativekafka/WordCounterApp.java
@@ -1,5 +1,6 @@
 package com.github.app.streams.nativekafka;
 
+import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.streams.KafkaStreams;
 import org.apache.kafka.streams.StreamsBuilder;
@@ -31,40 +32,30 @@ public class WordCounterApp {
     }
 
     public static Properties createProperties() {
-        final Properties streamsConfiguration = new Properties();
+        final Properties properties = new Properties();
         String bootstrapServers = "localhost:19092";
-
-        streamsConfiguration.put(StreamsConfig.APPLICATION_ID_CONFIG, "wordcount-lambda-example");
-        streamsConfiguration.put(StreamsConfig.CLIENT_ID_CONFIG, "wordcount-lambda-example-client");
-        streamsConfiguration.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
-        streamsConfiguration.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName());
-        streamsConfiguration.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName());
-        streamsConfiguration.put(StreamsConfig.COMMIT_INTERVAL_MS_CONFIG, 10 * 1000);
-        return streamsConfiguration;
+        properties.put(StreamsConfig.APPLICATION_ID_CONFIG, "native-kafka-app");
+        properties.put(StreamsConfig.CLIENT_ID_CONFIG, "native-kafka-client");
+        properties.put(ConsumerConfig.GROUP_ID_CONFIG, "native-kafka-group");
+        properties.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        properties.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName());
+        properties.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName());
+        return properties;
     }
 
     static void createWordCountStream(final StreamsBuilder builder) {
-        // Construct a `KStream` from the input topic "streams-plaintext-input", where message values
-        // represent lines of text (for the sake of this example, we ignore whatever may be stored
-        // in the message keys).  The default key and value serdes will be used.
+
         final KStream<String, String> textLines = builder.stream(INPUT_TOPIC);
 
         final Pattern pattern = Pattern.compile("\\W+", Pattern.UNICODE_CHARACTER_CLASS);
 
         final KTable<String, Long> wordCounts = textLines
-                // Split each text line, by whitespace, into words.  The text lines are the record
-                // values, i.e. we can ignore whatever data is in the record keys and thus invoke
-                // `flatMapValues()` instead of the more generic `flatMap()`.
-                .flatMapValues(value -> Arrays.asList(pattern.split(value.toLowerCase())))
-                // Group the split data by word so that we can subsequently count the occurrences per word.
-                // This step re-keys (re-partitions) the input data, with the new record key being the words.
-                // Note: No need to specify explicit serdes because the resulting key and value types
-                // (String and String) match the application's default serdes.
-                .groupBy((keyIgnored, word) -> word)
-                // Count the occurrences of each word (record key).
-                .count();
 
-        // Write the `KTable<String, Long>` to the output topic.
+                .flatMapValues(value -> Arrays.asList(pattern.split(value.toLowerCase())))
+
+                .groupBy((keyIgnored, word) -> word)
+
+                .count();
         wordCounts.toStream().to(OUTPUT_TOPIC, Produced.with(Serdes.String(), Serdes.Long()));
     }
 
