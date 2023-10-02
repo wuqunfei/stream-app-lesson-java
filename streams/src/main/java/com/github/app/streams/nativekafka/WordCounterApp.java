d35ebb1d63bc5d27f7a6d7ee2b9e0a0aa0a6aa6e
Qunfei Wu <wu.qunfei@gmail.com>
2023-10-02 08:28:47 +0200

    update the code to tun Main Application

--- streams/src/main/java/com/github/app/streams/nativekafka/WordCounterApp.java
+++ streams/src/main/java/com/github/app/streams/nativekafka/WordCounterApp.java
@@ -1,15 +1,18 @@
 package com.github.app.streams.nativekafka;
 
-import org.apache.kafka.common.serialization.Serde;
 import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.streams.KafkaStreams;
 import org.apache.kafka.streams.StreamsBuilder;
 import org.apache.kafka.streams.Topology;
-import org.apache.kafka.streams.kstream.*;
+import org.apache.kafka.streams.kstream.Consumed;
+import org.apache.kafka.streams.kstream.Grouped;
+import org.apache.kafka.streams.kstream.KStream;
+import org.apache.kafka.streams.kstream.KTable;
+import org.apache.kafka.streams.kstream.Materialized;
+import org.apache.kafka.streams.kstream.Produced;
 
 import java.util.Arrays;
 import java.util.Properties;
-
 
 import static org.apache.kafka.streams.StreamsConfig.APPLICATION_ID_CONFIG;
 import static org.apache.kafka.streams.StreamsConfig.BOOTSTRAP_SERVERS_CONFIG;
@@ -25,8 +28,8 @@ public class WordCounterApp {
         Topology topology = app.createTopology();       // Create streams transform Topology
         Properties properties = app.createProperty();   // Create connection property
 
-        try (KafkaStreams streams = new KafkaStreams(topology, properties)) {  // Create Stream application
-            Runtime.getRuntime().addShutdownHook(new Thread(streams::close));  // Close Application and close stream application Gracefully
+        try (KafkaStreams streams = new KafkaStreams(topology, properties)) {  // Create Stream application with Safety
+            Runtime.getRuntime().addShutdownHook(new Thread(streams::close));  // Close Application and stream application gracefully
             streams.start(); // Start stream application
         }
     }
@@ -35,19 +38,18 @@ public class WordCounterApp {
         String inputTopicName = "input-topic";
         String outputTopicName = "output-topic";
 
-        Serde<String> stringSerde = Serdes.String();
-        Serde<Long> longSerde = Serdes.Long();
-
         StreamsBuilder builder = new StreamsBuilder();
 
-        KStream<String, String> textLines = builder.stream(inputTopicName, Consumed.with(Serdes.String(), stringSerde));
-        KTable<String, Long> wordCounts = textLines
+        KStream<String, String> inputStream = builder.stream(inputTopicName, Consumed.with(Serdes.String(), Serdes.String()));
+
+        KTable<String, Long> wordCounts = inputStream
                 .flatMapValues(value -> Arrays.asList(value.toLowerCase().split("\\W+")))
                 .groupBy((key, word) -> word, Grouped.with(Serdes.String(), Serdes.String()))
                 .count(Materialized.as("counter-store"));
+
         KStream<String, Long> outputStream = wordCounts.toStream();
         // Convert the `KTable<String, Long>` into a `KStream<String, Long>` and write to the output topic.
-        outputStream.to(outputTopicName, Produced.with(stringSerde, longSerde));
+        outputStream.to(outputTopicName, Produced.with(Serdes.String(), Serdes.Long()));
         return builder.build();
     }
 
