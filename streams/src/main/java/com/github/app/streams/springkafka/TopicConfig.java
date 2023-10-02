0c6ad63d5358d50cc6bd538c1ee65007af3d7296
Qunfei Wu <wu.qunfei@gmail.com>
2023-10-02 09:45:18 +0200

    Adding application for 2 types 1. Cloud APP 2. Stream App 3. Adding input and out Topic

--- streams/src/main/java/com/github/app/streams/springkafka/TopicConfig.java
+++ streams/src/main/java/com/github/app/streams/springkafka/TopicConfig.java
@@ -1,6 +1,5 @@
 package com.github.app.streams.springkafka;
 
-import com.github.app.streams.springkafka.WordCounterProcessor;
 import org.apache.kafka.clients.admin.NewTopic;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
