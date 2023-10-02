5d1bff1cfd7007b329ac3683d49fb102ffdde504
Qunfei Wu <wu.qunfei@gmail.com>
2023-10-02 17:13:07 +0200

    fix native application

--- streams/src/main/java/com/github/app/streams/nativekafka/WordCounterApp.java
+++ streams/src/main/java/com/github/app/streams/nativekafka/WordCounterApp.java
@@ -3,62 +3,69 @@ package com.github.app.streams.nativekafka;
 import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.streams.KafkaStreams;
 import org.apache.kafka.streams.StreamsBuilder;
-import org.apache.kafka.streams.Topology;
-import org.apache.kafka.streams.kstream.Consumed;
-import org.apache.kafka.streams.kstream.Grouped;
+import org.apache.kafka.streams.StreamsConfig;
 import org.apache.kafka.streams.kstream.KStream;
 import org.apache.kafka.streams.kstream.KTable;
-import org.apache.kafka.streams.kstream.Materialized;
 import org.apache.kafka.streams.kstream.Produced;
 
 import java.util.Arrays;
 import java.util.Properties;
-
-import static org.apache.kafka.streams.StreamsConfig.APPLICATION_ID_CONFIG;
-import static org.apache.kafka.streams.StreamsConfig.BOOTSTRAP_SERVERS_CONFIG;
-import static org.apache.kafka.streams.StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG;
-import static org.apache.kafka.streams.StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG;
-
+import java.util.regex.Pattern;
 
 public class WordCounterApp {
-    public static void main(String[] args) {
 
-        WordCounterApp app = new WordCounterApp();
+    static final String INPUT_TOPIC = "input-native-topic";
+    static final String OUTPUT_TOPIC = "output-native-topic";
 
-        Topology topology = app.createTopology();       // Create streams transform Topology
-        Properties properties = app.createProperty();   // Create connection property
 
-        try (KafkaStreams streams = new KafkaStreams(topology, properties)) {  // Create Stream application with Safety
-            Runtime.getRuntime().addShutdownHook(new Thread(streams::close));  // Close Application and stream application gracefully
-            streams.start(); // Start stream application
-        }
-    }
+    public static void main(final String[] args) {
 
-    public Topology createTopology() {
-        String inputTopicName = "input-topic";
-        String outputTopicName = "output-topic";
 
+        Properties streamsConfiguration = createProperties();
         StreamsBuilder builder = new StreamsBuilder();
-
-        KStream<String, String> inputStream = builder.stream(inputTopicName, Consumed.with(Serdes.String(), Serdes.String()));
-
-        KTable<String, Long> wordCounts = inputStream
-                .flatMapValues(value -> Arrays.asList(value.toLowerCase().split("\\W+")))
-                .groupBy((key, word) -> word, Grouped.with(Serdes.String(), Serdes.String()))
-                .count(Materialized.as("counter-store"));
-
-        KStream<String, Long> outputStream = wordCounts.toStream();
-        // Convert the `KTable<String, Long>` into a `KStream<String, Long>` and write to the output topic.
-        outputStream.to(outputTopicName, Produced.with(Serdes.String(), Serdes.Long()));
-        return builder.build();
+        createWordCountStream(builder);
+        KafkaStreams streams = new KafkaStreams(builder.build(), streamsConfiguration);
+        streams.cleanUp();
+        streams.start();
+        Runtime.getRuntime().addShutdownHook(new Thread(streams::close));
     }
 
-    public Properties createProperty() {
-        Properties properties = new Properties();
-        properties.put(APPLICATION_ID_CONFIG, "native-kafka-app");
-        properties.put(BOOTSTRAP_SERVERS_CONFIG, "localhost:19092");
-        properties.put(DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass());
-        properties.put(DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass());
-        return properties;
+    public static Properties createProperties() {
+        final Properties streamsConfiguration = new Properties();
+        String bootstrapServers = "localhost:19092";
+
+        streamsConfiguration.put(StreamsConfig.APPLICATION_ID_CONFIG, "wordcount-lambda-example");
+        streamsConfiguration.put(StreamsConfig.CLIENT_ID_CONFIG, "wordcount-lambda-example-client");
+        streamsConfiguration.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        streamsConfiguration.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName());
+        streamsConfiguration.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName());
+        streamsConfiguration.put(StreamsConfig.COMMIT_INTERVAL_MS_CONFIG, 10 * 1000);
+        return streamsConfiguration;
     }
+
+    static void createWordCountStream(final StreamsBuilder builder) {
+        // Construct a `KStream` from the input topic "streams-plaintext-input", where message values
+        // represent lines of text (for the sake of this example, we ignore whatever may be stored
+        // in the message keys).  The default key and value serdes will be used.
+        final KStream<String, String> textLines = builder.stream(INPUT_TOPIC);
+
+        final Pattern pattern = Pattern.compile("\\W+", Pattern.UNICODE_CHARACTER_CLASS);
+
+        final KTable<String, Long> wordCounts = textLines
+                // Split each text line, by whitespace, into words.  The text lines are the record
+                // values, i.e. we can ignore whatever data is in the record keys and thus invoke
+                // `flatMapValues()` instead of the more generic `flatMap()`.
+                .flatMapValues(value -> Arrays.asList(pattern.split(value.toLowerCase())))
+                // Group the split data by word so that we can subsequently count the occurrences per word.
+                // This step re-keys (re-partitions) the input data, with the new record key being the words.
+                // Note: No need to specify explicit serdes because the resulting key and value types
+                // (String and String) match the application's default serdes.
+                .groupBy((keyIgnored, word) -> word)
+                // Count the occurrences of each word (record key).
+                .count();
+
+        // Write the `KTable<String, Long>` to the output topic.
+        wordCounts.toStream().to(OUTPUT_TOPIC, Produced.with(Serdes.String(), Serdes.Long()));
+    }
+
 }
